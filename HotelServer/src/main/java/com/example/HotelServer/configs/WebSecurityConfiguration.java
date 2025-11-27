ccccb36d7f52ed2cbad7156614d18a29438f3034
shalani <shalanikuruppu08@gmail.com>
2025-11-27 15:17:29 +0530

    feat: update pom.xml and implement JWT authentication in security configuration

--- HotelServer/src/main/java/com/example/HotelServer/configs/WebSecurityConfiguration.java
+++ HotelServer/src/main/java/com/example/HotelServer/configs/WebSecurityConfiguration.java
@@ -7,8 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.HotelServer.services.jwt.UserService;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -17,6 +23,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfiguration {
+
+        private final UserService userService;
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http.csrf(AbstractHttpConfigurer::disable)
@@ -29,5 +37,13 @@ public class WebSecurityConfiguration {
         @Bean
         public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
                 return config.getAuthenticationManager();
+        }
+
+        @Bean
+        public AuthenticationProvider authenticationProvider(){
+                DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+                authProvider.setUserDetailsService(userService.userDetailsService());
+                authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+                return authProvider;
         }
 }
--- HotelServer/src/main/java/com/example/HotelServer/utill/JwtUtil.java
+++ HotelServer/src/main/java/com/example/HotelServer/utill/JwtUtil.java
@@ -1,57 +1,60 @@
 package com.example.HotelServer.utill;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.Claims;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import java.util.Map;
+
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
-import java.security.Key;
+import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
-     private String generateToken(Map<String, Object> extraClaims, UserDetails details){
-            return Jwts.builder().setClaims(extraClaims).setSubject(details.getUsername()).setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                    .signWith(SignatureAlgorithm.HS256, getSigningKey()).compact();
-     }
 
-     public String generateToken(UserDetails userDetails){
-          return generateToken(new HashMap<>(), userDetails);
-     }
+    private String generateToken(Map<String, Object> extraClaims, UserDetails details){
+        return Jwts.builder().setClaims(extraClaims).setSubject(details.getUsername()).setIssuedAt(new Date(System.currentTimeMillis() + 1000*60*60*24)).setExpiration(new Date(System.currentTimeMillis())).signWith(SignatureAlgorithm.HS256, getSigningKey()).compact();
+    }
 
-     public boolean isTokenValid(String token, UserDetails userDetails){
-          final String username = extractUsername(token);
-          return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-     }
-     private Claims extractAllClaims(String token){
-          return Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(token).getBody();
-     }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
-     public String extractUsername(String token){
-          return extractClaim(token, Claims::getSubject);
-     }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userName = extractUserName(token);
 
-     private Date extractExpiration(String token){
-          return extractClaim(token, Claims::getExpiration);
-     }
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
-     private boolean isTokenExpired(String token){
-          return extractExpiration(token).before(new Date());
-     }
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
 
-     private <T> T extractClaim(String token, java.util.function.Function<Claims, T> claimsResolver){
-          final Claims claims = extractAllClaims(token);
-          return claimsResolver.apply(claims);
-     }
 
-     private Key getSigningKey() {
-          byte[] keyBytes = Decoders.BASE64.decode("404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970");
-          return Keys.hmacShaKeyFor(keyBytes);
-}
+    public String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
 
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode("413F4428472B4B6250655368566D5970337336763979244226452948404D6351");
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
